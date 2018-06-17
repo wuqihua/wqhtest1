07e8bd5f8e4e4d5a4280bce1dd1d5b38b3ffa895
wuqihua <15295556396@163.com>
2018-06-17 15:05:08 +0800

    allForFirst

---
+++ src/test/java/com/netease/wqh/account/Account.java
@@ -0,0 +1,43 @@
+package com.netease.wqh.account;
+
+public class Account {
+
+    private int accountId;
+
+    private String description;
+
+    private Long endDate;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Long endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId=" + accountId +
+                ", description='" + description + '\'' +
+                ", endDate=" + endDate +
+                '}';
+    }
+}
